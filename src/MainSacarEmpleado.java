38a5b037dee51aebe7cb897565730b7b98b038ec
azamatkhu <azamat.khu@educa.jcyl.es>
2026-04-21 13:19:39 +0200

    Bulk upload

--- src/MainSacarEmpleado.java
+++ src/MainSacarEmpleado.java
@@ -1,14 +1,28 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
-public class MainCrearEmpleado {
+public class MainSacarEmpleado {
     public static void main(String[] args) {
         try {
-            Connection connection = DriverManager.getConnection(DBConfig.getUrl(), DBConfig.getUser(), DBConfig.getPassword());
-
+            Connection connection = DriverManager.getConnection(
+                    "jdbc:oracle:thin:@localhost:1521:xe",
+                    "RIBERA",
+                    "ribera"
+            );
 
             System.out.println("Conectado!");
+
+            Statement statement = connection.createStatement();
+
+            String sql = "SELECT * FROM EMPLEADO WHERE MOD(ID, 2) != 0";
+            ResultSet resultSet = statement.executeQuery(sql);
+
+            while (resultSet.next()) {
+                System.out.println(resultSet.getInt("id") + " " + resultSet.getString("nombre") + " " + resultSet.getDouble("salario"));
+            }
+
+            System.out.println("Conectado!");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
 }
